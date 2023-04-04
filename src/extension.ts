77d6125ae56569c3971398c62b81d1032e1c50e1
ykisii <ishiiyuuki@gmail.com>
2023-04-04 16:23:10 +0900

    modify: activation events

--- src/extension.ts
+++ src/extension.ts
@@ -10,21 +10,6 @@ export function activate(context: vscode.ExtensionContext) {
 		console.log("onDidChangeActiveTextEditor");
 		textChanged();
 	}, null, context.subscriptions);
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "beacon" is now active!');
-
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('beacon.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from beacon!');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
