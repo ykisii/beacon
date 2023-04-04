af61b2bcd5403181aca335e814c3f62a16902282
ykisii <ishiiyuuki@gmail.com>
2023-04-04 11:14:56 +0900

    add: onDidChangeActiveTextEditor and refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { text } from 'stream/consumers';
 import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
+	vscode.window.onDidChangeActiveTextEditor(ediror => {
+		console.log("onDidChangeActiveTextEditor");
+		textChanged();
+	}, null, context.subscriptions);
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "beacon" is now active!');
@@ -18,7 +22,40 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from beacon!');
+		/*
 		const decorationType = vscode.window.createTextEditorDecorationType({
+			backgroundColor: 'red'
+		});
+		let editor = vscode.window.activeTextEditor;
+		let document = editor?.document;
+		let curPos: vscode.Position | undefined = editor?.selection.active;
+		console.log(curPos);
+		if (curPos) {
+			console.log(document?.offsetAt(curPos));
+			const pos = document?.offsetAt(curPos);
+			console.log("pos->", pos, pos !== undefined);
+			if (pos !== undefined) {
+				const line = editor?.document.lineAt?.(curPos);
+				console.log("line-> ", line, line?.range.start, line?.range.end);
+				const decoration = { range: new vscode.Range(line!.range.start, line!.range.end), hovermessage: "hooooo"};
+				editor?.setDecorations(decorationType, [decoration]);
+				//if (line) {
+				//	const decoration = { range: new vscode.Range(line.range.start, line.range.end), hovermessage: "hooooo"};
+				//	console.log("decoration-> ", decoration);
+				//	editor?.setDecorations(decorationType, [decoration]);
+				//}
+			}
+		}*/		
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
+
+function textChanged() {
+	const decorationType = vscode.window.createTextEditorDecorationType({
 			backgroundColor: 'red'
 		});
 		let editor = vscode.window.activeTextEditor;
@@ -40,11 +77,5 @@ export function activate(context: vscode.ExtensionContext) {
 					editor?.setDecorations(decorationType, [decoration]);
 				}*/
 			}
-		}		
-	});
-
-	context.subscriptions.push(disposable);
+		}	
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
